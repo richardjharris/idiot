2d1ca40671af8997dc8ae91c0630fef6363e67d8
Richard Harris <rjh@netcraft.com>
2020-06-07 23:03:35 +0100

    ImageManager: cache resized/rotated images
    Uses a clever/hideous (depending on your viewpoint) string prefix
    transformation syntax, so we cache all steps. We now load images
    on demand which speeds up the initial render.

--- ImageManager.java
+++ ImageManager.java
@@ -4,12 +4,10 @@ import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
 import javax.imageio.ImageIO;
 
 /** Holds all images. */
-// TODO cache resized images
 public class ImageManager {
   private static final String cardsPath = "cards2/";
   private static final String cardBackFilename =  "back2.png";
@@ -17,9 +15,11 @@ public class ImageManager {
   private static final String pointerFilename = "pointer.gif";
   private static final String burntFilename = "burnt.jpg";
 
-  private HashMap<String, BufferedImage> images;
+  // Caches images loaded from the jar. Populated on demand.
+  private HashMap<String, BufferedImage> cache = new HashMap<String, BufferedImage>();
+ 
+  // Preloaded four directions of pointer image
   private BufferedImage pointer[] = new BufferedImage[4];
-  private BufferedImage cardBackSideways;
 
   private static final int baseCardWidth = 73;
   private static final int baseCardHeight = 99;
@@ -27,75 +27,44 @@ public class ImageManager {
   private int cardWidth, cardHeight;
   private double scaleFactor;
 
+  // Public methods
+
   ImageManager(SHinterface sh) throws IOException {
     cardWidth = sh.scale(baseCardWidth);
     cardHeight = sh.scale(baseCardHeight);
     scaleFactor = sh.scaleFactor;
 
-    // List of files to load
-    ArrayList<String> files = new ArrayList<String>();
-    files.add(cardBackFilename);
-    files.add(titleFilename);
-    files.add(pointerFilename);
-    files.add(burntFilename);
-
-    for (String suit : Card.suits) {
-      for (String rank : Card.ranks) {
-        files.add(cardsPath + "500px-" + rank + suit + ".svg.png");
-      }
+    // Preload the rotated pointer images
+    String prefix = "";
+    for (int i = 0; i <= 3; i++) {
+      pointer[i] = get(prefix + pointerFilename);
+      prefix += "rotate$";
     }
-
-    images = new HashMap<String, BufferedImage>();
-    for (String file : files) {
-      URL url = this.getClass().getResource(file);
-      if (url == null) {
-        throw new Error("File " + file + " is missing or unreadable!");
-      }
-      BufferedImage image = ImageIO.read(url);
-      images.put(file, image);
-    }
-
-    // Produce rotated pointer images
-    pointer[0] = getImage(pointerFilename);
-    for (int i = 1; i <= 3; i++) {
-      pointer[i] = rotateClockwise90(pointer[i - 1]);
-    }
-
-    cardBackSideways = rotateClockwise90(getCardBack());
   }
 
-  BufferedImage getTitle() {
-    return getScaledImage(titleFilename, 1 + (scaleFactor - 1) * .5);
+  public BufferedImage getTitle() {
+    double scale = 1 + (scaleFactor - 1) * .5;
+    return get("scale" + scale + "$" + titleFilename);
   }
 
-  BufferedImage getBurnt() {
-    return getScaledImage(burntFilename, scaleFactor);
+  public BufferedImage getBurnt() {
+    return get("scale" + scaleFactor + "$" + burntFilename);
   }
 
-  BufferedImage getCardBack() {
-    return resize(getImage(cardBackFilename), cardWidth, cardHeight);
+  public BufferedImage getCardBack() {
+    return get("resize" + cardWidth + "x" + cardHeight + "$" + cardBackFilename);
   }
 
-  BufferedImage getCardBackSideways() {
-    return resize(cardBackSideways, cardHeight, cardWidth);
-  }
-
-  private BufferedImage getRawCardFront(int suit, int rank) {
-    // SH code starts at 2 and ends at 14 (Ace)
-    String rankString = Card.ranks[rank - 2];
-    // SH code starts at 1 (Hearts), 2 (Spades), 3 (Diamonds), 4 (Clubs)
-    String suitString = Card.suits[suit - 1];
-
-    return getImage(cardsPath + "500px-" + rankString + suitString + ".svg.png");
+  public BufferedImage getCardBackSideways() {
+    return get("rotate$resize" + cardWidth + "x" + cardHeight + "$" + cardBackFilename);
   }
 
   BufferedImage getCardFront(int suit, int rank) {
-    BufferedImage image = getRawCardFront(suit, rank);
-    return resize(image, cardWidth, cardHeight);
+    return get("resize" + cardWidth + "x" + cardHeight + "$" + getCardFilename(suit, rank));
   }
 
   BufferedImage getCardFrontSideways(int suit, int rank) {
-    return resize(rotateClockwise90(getRawCardFront(suit, rank)), cardHeight, cardWidth);
+    return get("rotate$resize" + cardWidth + "x" + cardHeight + "$" + getCardFilename(suit, rank));
   }
 
   /** Returns the pointer image, rotated for the specified player (0-3) */
@@ -103,13 +72,64 @@ public class ImageManager {
     return pointer[playerNo];
   }
 
-  private BufferedImage getImage(String filename) {
-    return images.get(filename);
+  // Private methods
+
+  /**
+   * Returns the image for the give cache key. The key is a filename within the
+   * application JAR, but may be prefixed with transformations separated by $,
+   * e.g. 'scale0.5$rotate$path/to/image.png'
+   * 
+   * Allowed transformations are:
+   *  scale0.5 (scale factor)
+   *  resize100x200 (width/height)
+   *  rotate (clockwise 90)
+   */
+  private BufferedImage get(String key) {
+    // Check cache first
+    if (cache.containsKey(key)) {
+      return cache.get(key);
+    }
+
+    BufferedImage dest = null;
+    int dollar = key.indexOf('$');
+    if (dollar != -1) {
+      // Pop the transformation off
+      String transform = key.substring(0, dollar);
+      BufferedImage src = get(key.substring(dollar + 1));
+      if (transform.startsWith("scale")) {
+        double scaleBy = new Double(transform.substring(5));
+        dest = scale(src, scaleBy);
+      } else if (transform.startsWith("resize")) {
+        String[] wh = transform.substring(6).split("x");
+        dest = resize(src, new Integer(wh[0]), new Integer(wh[1]));
+      } else if (transform.equals("rotate")) {
+        dest = rotateClockwise90(src);
+      }
+    } else {
+      // Raw filename
+      URL url = this.getClass().getResource(key);
+      if (url == null) {
+        throw new Error("File '" + key + "' is missing or unreadable!");
+      }
+      try {
+        dest = ImageIO.read(url);
+      } catch(IOException e) {
+        throw new Error("Failed to open file '" + url + "'");
+      }
+    }
+
+    // Store in cache
+    cache.put(key, dest);
+    return dest;
   }
 
-  private BufferedImage getScaledImage(String filename, double scale) {
-    BufferedImage img = getImage(filename);
-    return resize(img, (int)(img.getWidth() * scale), (int)(img.getHeight() * scale));
+  private static String getCardFilename(int suit, int rank) {
+    // SH code starts at 2 and ends at 14 (Ace)
+    String rankString = Card.ranks[rank - 2];
+    // SH code starts at 1 (Hearts), 2 (Spades), 3 (Diamonds), 4 (Clubs)
+    String suitString = Card.suits[suit - 1];
+
+    return cardsPath + "500px-" + rankString + suitString + ".svg.png";
   }
 
   // Return a rotated copy of the image (clockwise 90 degrees)
@@ -130,19 +150,20 @@ public class ImageManager {
     return dest;
   }
 
+  // Scale an image by the specified scale factor.
+  static BufferedImage scale(BufferedImage src, double scaleFactor) {
+    int newWidth = (int)(src.getWidth() * scaleFactor);
+    int newHeight = (int)(src.getHeight() * scaleFactor);
+    return resize(src, newWidth, newHeight);
+  }
+
+  // Resize an image to the specified width/height. Uses a special algorithm
+  // optimised for down-scaling.
   static BufferedImage resize(BufferedImage src, int newWidth, int newHeight) {
     int type = src.getType() != 0 ? src.getType() : 5;
     BufferedImage dest = new BufferedImage(newWidth, newHeight, type);
     progressiveResize(src, dest);
     return dest;
-  }
-
-  static private Graphics2D createGraphics(BufferedImage img) {
-    Graphics2D g = img.createGraphics();
-    g.setRenderingHint(
-      RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		return g;  
   }
 
 	// Progressive resizer, stolen from Thumbnailator library.
@@ -224,5 +245,14 @@ public class ImageManager {
 		Graphics2D destg = createGraphics(destImage);
 		destg.drawImage(tempImage, 0, 0, targetWidth, targetHeight, 0, 0, currentWidth, currentHeight, null);
 		destg.dispose();
-	}
+  }
+  
+  static private Graphics2D createGraphics(BufferedImage img) {
+    Graphics2D g = img.createGraphics();
+    g.setRenderingHint(
+      RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		return g;  
+  }
+
 }
